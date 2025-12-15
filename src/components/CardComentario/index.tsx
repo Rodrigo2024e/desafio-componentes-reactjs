a30c44c9a406402427fdab78de0822fb5d115300
Rodrigo <lucianorodrigocarvalho@gmail.com>
2025-12-15 07:22:21 -0300

    Card-Comentário

---
+++ src/components/CardComentario/index.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function CardComentario() {
+  return (
+    
+      <div className="ct-comentario">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipi. Vitae harum voluptatem ad fugit id distinctio.
+        </p>
+      </div>
+  
+  );
+}
--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,4 +1,6 @@
 import Card from "../../components/Card";
+import CardComentario from "../../components/CardComentario";
+
 import Header from "../../components/Header";
 
 export default function Catalog() {
@@ -7,11 +9,22 @@ export default function Catalog() {
       <Header />
       <main className="ct-catalog-main">
         <section id="ct-catalog-section" className="ct-container">
-            <h2>Venha nos visitar</h2>
+          <h2>Venha nos visitar</h2>
           <div className="ct-catalog-cards-container">
             <Card />
             <Card />
-          
+          </div>
+        </section>
+
+        <section id="ct-comentario-section" className="ct-container-comentario">
+          <h2>O que estão dizendo</h2>
+          <div className="ct-catalog-card-comentario">
+            <CardComentario />
+            <CardComentario />
+            <CardComentario />
+            <CardComentario />
+            <CardComentario />
+            <CardComentario />
           </div>
         </section>
       </main>
